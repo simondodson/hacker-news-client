5e42ab46ba229f4d463652c946b6750cf7d51c1d
SIMON DODSON <simondodson@gmail.com>
2020-11-01 09:54:50 +0700

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -304,27 +304,13 @@ export default function App({ children }: { children: ReactNode }) {
               target='_blank'
               style={{ bottom: '0.19rem' }}
             >
-              <span className='sr-only'>Bhanu Teja Pachipulusu's Blog</span>
-              <img
-                className='w-8 opacity-30 hover:opacity-100'
-                src='/b-logo.png'
-              />
-            </a>
+  
             <a
               href='https://www1.com.au'
               className='text-gray-400 hover:text-gray-500'
               target='_blank'
             >
-              <span className='sr-only'>
-                Bhanu Teja Pachipulusu's Personal Website
-              </span>
-              <svg
-                className='w-6 h-6'
-                xmlns='http://www.w3.org/2000/svg'
-                fill='none'
-                viewBox='0 0 24 24'
-                stroke='currentColor'
-              >
+      
                 <path
                   strokeLinecap='round'
                   strokeLinejoin='round'
@@ -333,26 +319,7 @@ export default function App({ children }: { children: ReactNode }) {
                 />
               </svg>
             </a>
-            <a
-              href='https://hashnode.com/@pbteja1998'
-              className='text-gray-400 hover:text-gray-500'
-              target='_blank'
-              rel='noopener noreferrer nofollow'
-            >
-              <span className='sr-only'>Hashnode</span>
-              <svg
-                className='w-6 h-6'
-                aria-hidden='true'
-                viewBox='0 0 337 337'
-                fill='currentColor'
-              >
-                <path
-                  fillRule='evenodd'
-                  clipRule='evenodd'
-                  d='M23.155 112.598c-30.873 30.874-30.873 80.93 0 111.804l89.443 89.443c30.874 30.873 80.93 30.873 111.804 0l89.443-89.443c30.873-30.874 30.873-80.93 0-111.804l-89.443-89.443c-30.874-30.873-80.93-30.873-111.804 0l-89.443 89.443zm184.476 95.033c21.612-21.611 21.612-56.651 0-78.262-21.611-21.612-56.651-21.612-78.262 0-21.612 21.611-21.612 56.651 0 78.262 21.611 21.612 56.651 21.612 78.262 0z'
-                />
-              </svg>
-            </a>
+  
             <a
               href='https://twitter.com/simondodson'
               className='text-gray-400 hover:text-gray-500'
@@ -369,32 +336,13 @@ export default function App({ children }: { children: ReactNode }) {
                 <path d='M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84' />
               </svg>
             </a>
-            <a
-              href='https://github.com/pbteja1998'
-              className='text-gray-400 hover:text-gray-500'
-              target='_blank'
-              rel='noopener noreferrer nofollow'
-            >
-              <span className='sr-only'>GitHub</span>
-              <svg
-                className='w-6 h-6'
-                aria-hidden='true'
-                fill='currentColor'
-                viewBox='0 0 24 24'
-              >
-                <path
-                  fillRule='evenodd'
-                  d='M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z'
-                  clipRule='evenodd'
-                />
-              </svg>
-            </a>
+        
         
           </div>
           <div className='mt-2 md:mt-0 md:order-1'>
             <p className='text-base leading-6 prose text-center text-gray-400'>
               &copy; 2020 Made with 🧡 by{'sydneysnews'}
-              <a href='http://goodpizza.com.au'>goodpizza.com.au</a>.
+              <a href='http://goodpizza.com.au'> goodpizza.com.au</a>.
             </p>
           </div>
         </div>
